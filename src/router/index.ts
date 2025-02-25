06a462697b3c89b2a4c6e444f956cde4392e5619
AtigerLion <w5830155@sina.cn>
2025-02-25 09:58:45 +0000

    更新env.d.ts文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
